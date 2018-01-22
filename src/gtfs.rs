4263e372acc3031ab03c9411c131c68cfe9f0b92
Pascal Rhod <pascal.rhod@canaltp.fr>
2018-01-22 22:01:52 +0100

    gtfs - fmt

--- src/gtfs.rs
+++ src/gtfs.rs
@@ -81,11 +81,9 @@ struct Stop {
 }
 impl From<Stop> for objects::StopArea {
     fn from(stop: Stop) -> objects::StopArea {
-        let mut stop_codes : Vec<(String, String)> = vec![];
+        let mut stop_codes: Vec<(String, String)> = vec![];
         if let Some(c) = stop.code {
-            stop_codes.push(
-                ("gtfs_stop_code".to_string(), c)
-            );
+            stop_codes.push(("gtfs_stop_code".to_string(), c));
         }
         objects::StopArea {
             id: stop.id,
@@ -107,11 +105,9 @@ impl From<Stop> for objects::StopPoint {
     fn from(stop: Stop) -> objects::StopPoint {
         let id = stop.id;
         let stop_area_id = stop.parent_station.unwrap_or_else(|| format!("SA{}", id));
-        let mut stop_codes : Vec<(String, String)> = vec![];
+        let mut stop_codes: Vec<(String, String)> = vec![];
         if let Some(c) = stop.code {
-            stop_codes.push(
-                ("gtfs_stop_code".to_string(), c)
-            );
+            stop_codes.push(("gtfs_stop_code".to_string(), c));
         }
         objects::StopPoint {
             id: id,
