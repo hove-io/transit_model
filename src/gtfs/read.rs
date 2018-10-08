6f04d8f676876ac2c3c638d4d58ce47400b33279
David Quintanel <david.quintanel@canaltp.fr>
2018-10-08 10:22:24 +0200

    [ntfs2gtfs] write agency.txt

--- src/gtfs/read.rs
+++ src/gtfs/read.rs
@@ -30,6 +30,7 @@ use std::result::Result as StdResult;
 use utils::*;
 use Result;
 extern crate serde_json;
+use super::Agency;
 
 fn default_agency_id() -> String {
     "default_agency_id".to_string()
@@ -47,23 +48,6 @@ fn get_agency_id(route: &Route, networks: &CollectionWithId<objects::Network>) -
         })
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Agency {
-    #[serde(rename = "agency_id")]
-    id: Option<String>,
-    #[serde(rename = "agency_name")]
-    name: String,
-    #[serde(rename = "agency_url")]
-    url: String,
-    #[serde(rename = "agency_timezone")]
-    timezone: Option<String>,
-    #[serde(rename = "agency_lang")]
-    lang: Option<String>,
-    #[serde(rename = "agency_phone")]
-    phone: Option<String>,
-    #[serde(rename = "agency_email")]
-    email: Option<String>,
-}
 impl From<Agency> for objects::Network {
     fn from(agency: Agency) -> objects::Network {
         objects::Network {
--- src/gtfs/write.rs
+++ src/gtfs/write.rs
@@ -13,3 +13,95 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see
 // <http://www.gnu.org/licenses/>.
+
+use super::Agency;
+use collection::CollectionWithId;
+use csv;
+use failure::ResultExt;
+use objects::*;
+use std::path;
+use Result;
+
+pub fn write_agencies(path: &path::Path, networks: &CollectionWithId<Network>) -> Result<()> {
+    info!("Writing agency.txt");
+    let path = path.join("agency.txt");
+    let mut wtr = csv::Writer::from_path(&path).with_context(ctx_from_path!(path))?;
+    for n in networks.values() {
+        wtr.serialize(Agency {
+            id: Some(n.id.clone()),
+            name: n.name.clone(),
+            url: n
+                .url
+                .clone()
+                .unwrap_or("http://www.navitia.io/".to_string()),
+            timezone: n.timezone.clone().or(Some("Europe/Paris".to_string())),
+            lang: n.lang.clone(),
+            phone: n.phone.clone(),
+            email: None,
+        }).with_context(ctx_from_path!(path))?;
+    }
+
+    wtr.flush().with_context(ctx_from_path!(path))?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Agency;
+    use objects::Network;
+
+    #[test]
+    fn write_agency() {
+        let agency = Agency::from(Network {
+            id: "OIF:101".to_string(),
+            name: "SAVAC".to_string(),
+            url: Some("http://www.vianavigo.com,Europe/Paris".to_string()),
+            timezone: Some("Europe/Madrid".to_string()),
+            lang: Some("fr".to_string()),
+            phone: Some("0123456789".to_string()),
+            address: Some("somewhere".to_string()),
+            sort_order: Some(1),
+            codes: vec![],
+        });
+
+        let expected_agency = Agency {
+            id: Some("OIF:101".to_string()),
+            name: "SAVAC".to_string(),
+            url: "http://www.vianavigo.com,Europe/Paris".to_string(),
+            timezone: Some("Europe/Madrid".to_string()),
+            lang: Some("fr".to_string()),
+            phone: Some("0123456789".to_string()),
+            email: None,
+        };
+
+        assert_eq!(expected_agency, agency);
+    }
+
+    #[test]
+    fn write_agency_with_default_values() {
+        let agency = Agency::from(Network {
+            id: "OIF:101".to_string(),
+            name: "SAVAC".to_string(),
+            url: None,
+            timezone: None,
+            lang: None,
+            phone: None,
+            address: None,
+            sort_order: None,
+            codes: vec![],
+        });
+
+        let expected_agency = Agency {
+            id: Some("OIF:101".to_string()),
+            name: "SAVAC".to_string(),
+            url: "http://www.navitia.io/".to_string(),
+            timezone: Some("Europe/Paris".to_string()),
+            lang: None,
+            phone: None,
+            email: None,
+        };
+
+        assert_eq!(expected_agency, agency);
+    }
+}
