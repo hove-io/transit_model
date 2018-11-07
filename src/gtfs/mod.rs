7fb6f207e9753dc4a22a67a97ce517a890c6b1e2
David Quintanel <david.quintanel@canaltp.fr>
2018-11-07 16:40:50 +0100

    Added link to documentation NTFS to GTFS conversion

--- src/gtfs/mod.rs
+++ src/gtfs/mod.rs
@@ -256,6 +256,7 @@ where
 
 /// Exports a `Model` to [GTFS](http://gtfs.org/) files
 /// in the given directory.
+/// see [NTFS to GTFS conversion](https://github.com/CanalTP/navitia_model/blob/master/src/documentation/ntfs2gtfs.md)
 pub fn write<P: AsRef<Path>>(model: &Model, path: P) -> Result<()> {
     let path = path.as_ref();
     info!("Writing GTFS to {:?}", path);
