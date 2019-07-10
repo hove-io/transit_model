82358f27ef03839b0659ca32c509df78c5a29667
Jean SIMARD <woshilapin@tuziwo.info>
2019-07-10 11:49:49 +0200

    Add 'transxchange2ntfs' binary under a featured compilation

---
+++ src/bin/transxchange2ntfs.rs
@@ -0,0 +1,75 @@
+// Copyright 2017 Kisio Digital and/or its affiliates.
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+use chrono::NaiveDateTime;
+use log::info;
+use std::path::PathBuf;
+use structopt;
+use structopt::StructOpt;
+use transit_model::Result;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "transxchange2ntfs",
+    about = "Convert a TransXChange to an NTFS."
+)]
+struct Opt {
+    /// input directory containing TransXChange files
+    #[structopt(long, short, parse(from_os_str), default_value = ".")]
+    input: PathBuf,
+
+    /// input directory containing NAPTAN files
+    #[structopt(long, short, parse(from_os_str), default_value = ".")]
+    naptan: PathBuf,
+
+    /// output directory for the NTFS files
+    #[structopt(short, long, parse(from_os_str))]
+    output: PathBuf,
+
+    /// prefix
+    #[structopt(short, long)]
+    prefix: Option<String>,
+
+    /// current datetime
+    #[structopt(
+        short = "x",
+        long,
+        parse(try_from_str),
+        raw(default_value = "&transit_model::CURRENT_DATETIME")
+    )]
+    current_datetime: NaiveDateTime,
+}
+
+fn run() -> Result<()> {
+    info!("Launching transxchange2ntfs...");
+
+    let opt = Opt::from_args();
+
+    let model = transit_model::transxchange::read(opt.input, opt.naptan)?;
+
+    transit_model::ntfs::write(&model, opt.output, opt.current_datetime)?;
+    Ok(())
+}
+
+fn main() {
+    env_logger::init();
+    if let Err(err) = run() {
+        for cause in err.iter_chain() {
+            eprintln!("{}", cause);
+        }
+        std::process::exit(1);
+    }
+}
