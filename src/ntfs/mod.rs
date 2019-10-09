b2d2d2ac67b355e106ab4871d9a23c1b92981f5f
patoche <patrick.degeilh@kisio.com>
2019-10-09 09:47:52 +0200

    fix fmt error

--- src/ntfs/mod.rs
+++ src/ntfs/mod.rs
@@ -299,11 +299,11 @@ pub fn write_to_zip<P: AsRef<path::Path>>(
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::{read_utils::PathFileHandler, test_utils::*};
     use super::Collections;
+    use super::*;
     use super::{read, write};
     use crate::common_format::{manage_calendars, write_calendar_dates, Availability};
+    use crate::{read_utils::PathFileHandler, test_utils::*};
     use geo_types::line_string;
     use pretty_assertions::assert_eq;
     use serde;
