7e55f2b4c39c9e4bb7785b2ff186147a352a9362
Jean SIMARD <woshilapin@tuziwo.info>
2020-01-10 11:43:05 +0100

    [tech] Skeleton for the Netex France exporter

---
+++ src/netex_france/mod.rs
@@ -0,0 +1,19 @@
+// Copyright (C) 2017 Kisio Digital and/or its affiliates.
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by the
+// Free Software Foundation, version 3.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>
+
+//! Module to handle Netex France profile
+
+mod exporter;
+
+pub use exporter::Exporter;
