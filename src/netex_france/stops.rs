f2f90c2f1cc7943760a05c4fabef2872f9579499
Jean SIMARD <woshilapin@tuziwo.info>
2020-01-24 17:03:55 +0100

    [feature] Export StopPlace for NeTEx France

--- src/netex_france/stops.rs
+++ src/netex_france/stops.rs
@@ -12,16 +12,113 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>
 
-use crate::{objects::StopPoint, Model, Result};
+use crate::{
+    objects::{Coord, StopArea, StopPoint},
+    Model, Result,
+};
 use failure::format_err;
 use minidom::{Element, Node};
 use proj::Proj;
+use std::{
+    borrow::Borrow,
+    collections::{BTreeSet, HashMap},
+    fmt::{self, Display, Formatter},
+};
 
+#[derive(Debug)]
+enum StopType {
+    Quay,
+    MonomodalStopPlace,
+    MultimodalStopPlace,
+}
+
+impl Display for StopType {
+    fn fmt(&self, f: &mut Formatter) -> std::result::Result<(), fmt::Error> {
+        use StopType::*;
+        match self {
+            Quay => write!(f, "ZE"),
+            MonomodalStopPlace => write!(f, "LMO"),
+            MultimodalStopPlace => write!(f, "LMU"),
+        }
+    }
+}
+
+// For the order, see
+// https://github.com/CanalTP/ntfs-specification/blob/v0.11.0/ntfs_fr.md#physical_modestxt-requis
+// Note that 2 enum cannot have the same value so `Funicular` and `Cableway`
+// have different values. Same for `Coach` and `Bus`.
+#[derive(Debug, Eq, PartialEq, PartialOrd, Ord, Clone, Copy)]
+enum NetexMode {
+    Air = 1,
+    Water = 2,
+    Rail = 3,
+    Metro = 4,
+    Tram = 5,
+    Funicular = 6,
+    Cableway = 7,
+    Coach = 8,
+    Bus = 9,
+}
+
+impl Display for NetexMode {
+    fn fmt(&self, f: &mut Formatter) -> std::result::Result<(), fmt::Error> {
+        use NetexMode::*;
+        match self {
+            Air => write!(f, "air"),
+            Bus => write!(f, "bus"),
+            Cableway => write!(f, "cableway"),
+            Coach => write!(f, "coach"),
+            Funicular => write!(f, "funicular"),
+            Metro => write!(f, "metro"),
+            Rail => write!(f, "rail"),
+            Tram => write!(f, "tram"),
+            Water => write!(f, "water"),
+        }
+    }
+}
+
+impl NetexMode {
+    fn from_physical_mode_id(physical_mode_id: &str) -> Option<NetexMode> {
+        use NetexMode::*;
+        match physical_mode_id {
+            "Air" => Some(Air),
+            "Boat" => Some(Water),
+            "Bus" => Some(Bus),
+            "BusRapidTransit" => Some(Bus),
+            "Coach" => Some(Coach),
+            "Ferry" => Some(Water),
+            "Funicular" => Some(Funicular),
+            "LocalTrain" => Some(Rail),
+            "LongDistanceTrain" => Some(Rail),
+            "Metro" => Some(Metro),
+            "RapidTransit" => Some(Rail),
+            "RailShuttle" => Some(Rail),
+            "Shuttle" => Some(Bus),
+            "SuspendedCableCar" => Some(Cableway),
+            "Train" => Some(Rail),
+            "Tramway" => Some(Tram),
+            _ => None,
+        }
+    }
+}
+
+// `stop_point_modes` is storing all the modes for a StopPoint.
+//
+// A Stop Point can have multiple associated modes in NTM model. We use a
+// `BTreeSet` for determinism of the order (so fixtures are always written in
+// the same order).
+//
+// Processing of `stop_point_modes` information is expansive which is the reason
+// why we process it at construction of `StopExporter` and then store it.
+type StopPointModes<'a> = HashMap<&'a str, BTreeSet<NetexMode>>;
+type StopAreaStopPoints<'a> = HashMap<&'a str, BTreeSet<&'a str>>;
 pub struct StopExporter<'a> {
     model: &'a Model,
     participant_ref: &'a str,
     stop_provider_code: &'a str,
     converter: Proj,
+    stop_point_modes: StopPointModes<'a>,
+    stop_area_stop_points: StopAreaStopPoints<'a>,
 }
 
 // Publicly exposed methods
@@ -37,56 +134,206 @@ impl<'a> StopExporter<'a> {
         let converter = Proj::new_known_crs(from, to, None).ok_or_else(|| {
             format_err!("Proj cannot build a converter from '{}' to '{}'", from, to)
         })?;
+        let stop_point_modes = Self::build_stop_point_modes(model);
+        let stop_area_stop_points = Self::build_stop_area_stop_points(model);
         let exporter = StopExporter {
             model,
             participant_ref,
             stop_provider_code,
             converter,
+            stop_point_modes,
+            stop_area_stop_points,
         };
         Ok(exporter)
     }
     pub fn export(&self) -> Result<Vec<Element>> {
-        self.model
+        let stop_points_elements = self
+            .model
             .stop_points
             .values()
             .map(|stop_point| self.export_stop_point(stop_point))
-            .collect()
+            .collect::<Result<Vec<Element>>>()?;
+        let stop_areas_elements = self
+            .model
+            .stop_areas
+            .values()
+            .map(|stop_area| self.export_stop_area(stop_area))
+            .collect::<Result<Vec<Vec<Element>>>>()?;
+        let mut elements = stop_points_elements;
+        elements.extend(stop_areas_elements.into_iter().flatten());
+        Ok(elements)
     }
 }
 
 // Internal methods
 impl<'a> StopExporter<'a> {
+    // To find the mode associated to a Stop Area, here is the following
+    // sequence of actions:
+    // - we need to iterate over all Vehicle Journeys
+    // - convert the Physical Mode into a NeTEx mode
+    // - iterate over all Stop Times in these Vehicle Journeys (this one is
+    //   expansive)
+    // - find the corresponding Stop Point
+    // - find the corresponding parent Stop Area
+    fn build_stop_point_modes(model: &'a Model) -> StopPointModes<'a> {
+        model
+            .vehicle_journeys
+            .values()
+            .filter_map(|vehicle_journey| {
+                NetexMode::from_physical_mode_id(&vehicle_journey.physical_mode_id)
+                    .map(move |netex_mode| (vehicle_journey, netex_mode))
+            })
+            .flat_map(|(vehicle_journey, netex_mode)| {
+                vehicle_journey
+                    .stop_times
+                    .iter()
+                    .map(|stop_time| &stop_time.stop_point_idx)
+                    .map(|stop_point_idx| &model.stop_points[*stop_point_idx])
+                    .map(move |stop_point| (&stop_point.id, netex_mode))
+            })
+            .fold(
+                HashMap::new(),
+                |mut stop_point_modes, (stop_point_id, netex_mode)| {
+                    stop_point_modes
+                        .entry(stop_point_id)
+                        .or_insert_with(BTreeSet::new)
+                        .insert(netex_mode);
+                    stop_point_modes
+                },
+            )
+    }
+
+    fn build_stop_area_stop_points(model: &'a Model) -> StopAreaStopPoints<'a> {
+        model
+            .stop_points
+            .values()
+            .fold(HashMap::new(), |mut stop_area_stop_points, stop_point| {
+                stop_area_stop_points
+                    .entry(&stop_point.stop_area_id)
+                    .or_insert_with(BTreeSet::new)
+                    .insert(&stop_point.id);
+                stop_area_stop_points
+            })
+    }
+
     fn export_stop_point(&self, stop_point: &'a StopPoint) -> Result<Element> {
         let element_builder = Element::builder("Quay")
-            .attr("id", self.generate_id(stop_point))
+            .attr("id", self.generate_id(&stop_point.id, StopType::Quay, None))
             .attr("version", "any");
-        let element_builder = element_builder.append(self.generate_name(stop_point));
-        let element_builder = if let Some(public_code) = self.generate_public_code(stop_point) {
-            element_builder.append(public_code)
+        let element_builder = element_builder.append(self.generate_name(&stop_point.name));
+        let element_builder = element_builder.append(self.generate_centroid(&stop_point.coord)?);
+        // TODO: Find most frequent 'physical_mode' for 'stop_point' after converting to NeTEx modes
+        // let element_builder = element_builder.append(self.generate_transport_mode(stop_point));
+        let element_builder = if let Some(tariff_zones) = self.generate_tariff_zones(stop_point) {
+            element_builder.append(tariff_zones)
         } else {
             element_builder
         };
-        let element_builder = element_builder.append(self.generate_site_ref(stop_point));
-        let element_builder = element_builder.append(self.generate_centroid(stop_point)?);
-        let element_builder = element_builder.append(self.generate_transport_mode(stop_point));
-        let element_builder = if let Some(tariff_zones) = self.generate_tariff_zones(stop_point) {
-            element_builder.append(tariff_zones)
+        let element_builder = if let Some(public_code) = self.generate_public_code(stop_point) {
+            element_builder.append(public_code)
         } else {
             element_builder
         };
         Ok(element_builder.build())
     }
 
-    fn generate_id(&self, stop_point: &'a StopPoint) -> String {
-        let id = stop_point.id.replace(':', "_");
-        // TODO: Find INSEE code from geolocation of the `stop_point`
-        let insee = "XXXXX";
-        format!("FR:{}:ZE:{}:{}", insee, id, self.stop_provider_code)
+    fn export_stop_area(&self, stop_area: &'a StopArea) -> Result<Vec<Element>> {
+        if let Some(stop_point_ids) = self.stop_area_stop_points.get(stop_area.id.as_str()) {
+            let netex_modes: BTreeSet<NetexMode> = stop_point_ids
+                .iter()
+                .filter_map(|stop_point_id| self.model.stop_points.get(stop_point_id))
+                .filter_map(|stop_point| self.stop_point_modes.get(stop_point.id.as_str()))
+                .flatten()
+                .copied()
+                .collect();
+            let mut stop_place_elements = Vec::new();
+            let name_element = self.generate_name(&stop_area.name);
+            let parent_station_id =
+                self.generate_id(&stop_area.id, StopType::MultimodalStopPlace, None);
+            let parent_site_ref_element = self.generate_parent_site_ref(&parent_station_id);
+            let centroid = self.generate_centroid(&stop_area.coord)?;
+            for netex_mode in &netex_modes {
+                // Get only Stop Points with the current NeTEx mode
+                let stop_point_ids = stop_point_ids
+                    .iter()
+                    .filter(|&stop_point_id| {
+                        self.stop_point_modes
+                            .get(stop_point_id)
+                            .map(|netex_modes| netex_modes.contains(netex_mode))
+                            .unwrap_or(false)
+                    })
+                    .collect::<BTreeSet<_>>();
+                let element_builder = Element::builder("StopPlace")
+                    .attr(
+                        "id",
+                        self.generate_id(
+                            &stop_area.id,
+                            StopType::MonomodalStopPlace,
+                            Some(*netex_mode),
+                        ),
+                    )
+                    .attr("version", "any");
+                let element_builder = element_builder.append(name_element.clone());
+                let element_builder = element_builder.append(centroid.clone());
+                let element_builder = element_builder.append(parent_site_ref_element.clone());
+                let element_builder =
+                    element_builder.append(self.generate_transport_mode(*netex_mode));
+                let element_builder =
+                    element_builder.append(self.generate_stop_place_type(*netex_mode));
+                let element_builder = element_builder.append(self.generate_quays(stop_point_ids));
+                stop_place_elements.push(element_builder.build());
+            }
+            let element_builder = Element::builder("StopPlace")
+                .attr(
+                    "id",
+                    self.generate_id(&stop_area.id, StopType::MultimodalStopPlace, None),
+                )
+                .attr("version", "any");
+            let element_builder = element_builder.append(name_element);
+            let element_builder = element_builder.append(centroid);
+            let highest_netex_mode =
+                self.calculate_highest_mode(&netex_modes).ok_or_else(|| {
+                    // Should never happen, a Stop Area always have at least one associated mode
+                    format_err!(
+                        "Unable to resolve main NeTEx mode for Stop Area {}",
+                        stop_area.id
+                    )
+                })?;
+            let element_builder =
+                element_builder.append(self.generate_transport_mode(highest_netex_mode));
+            let element_builder =
+                element_builder.append(self.generate_stop_place_type(highest_netex_mode));
+            stop_place_elements.push(element_builder.build());
+            Ok(stop_place_elements)
+        } else {
+            Ok(Vec::new())
+        }
     }
 
-    fn generate_name(&self, stop_point: &'a StopPoint) -> Element {
+    fn generate_id(
+        &self,
+        id: &'a str,
+        stop_type: StopType,
+        netex_mode: Option<NetexMode>,
+    ) -> String {
+        let id = id.replace(':', "_");
+        let id = if let Some(netex_mode) = netex_mode {
+            format!("{}_{}", id, netex_mode)
+        } else {
+            id
+        };
+
+        // TODO: Find INSEE code from geolocation of the `stop_point`
+        let insee = "XXXXX";
+        format!(
+            "FR:{}:{}:{}:{}",
+            insee, stop_type, id, self.stop_provider_code
+        )
+    }
+
+    fn generate_name(&self, name: &'a str) -> Element {
         Element::builder("Name")
-            .append(Node::Text(stop_point.name.to_owned()))
+            .append(Node::Text(name.to_owned()))
             .build()
     }
 
@@ -98,8 +345,8 @@ impl<'a> StopExporter<'a> {
         })
     }
 
-    fn generate_centroid(&self, stop_point: &'a StopPoint) -> Result<Element> {
-        let coord_epsg2154 = self.converter.convert(stop_point.coord)?;
+    fn generate_centroid(&self, coord: &'a Coord) -> Result<Element> {
+        let coord_epsg2154 = self.converter.convert(*coord)?;
         let coord_text = Node::Text(format!("{} {}", coord_epsg2154.x(), coord_epsg2154.y()));
         let pos = Element::builder("gml:pos")
             .attr("srsName", "EPSG:2154")
@@ -110,21 +357,22 @@ impl<'a> StopExporter<'a> {
         Ok(centroid)
     }
 
-    fn generate_site_ref(&self, _stop_point: &'a StopPoint) -> Element {
-        // TODO: Figure out the identifier of the parent stop
-        let parent_site_ref = "";
+    fn generate_parent_site_ref(&self, parent_station_id: &'a str) -> Element {
         Element::builder("ParentSiteRef")
-            .attr("ref", parent_site_ref)
+            .attr("ref", parent_station_id)
             .build()
     }
 
-    fn generate_transport_mode(&self, _stop_point: &'a StopPoint) -> Element {
-        // TODO: Find most frequent 'physical_mode' for 'stop_point' after converting to NeTEx modes
-        let mode = String::new();
-        let transport_mode_text = Node::Text(mode);
+    fn generate_transport_mode(&self, netex_mode: NetexMode) -> Element {
+        let transport_mode_text = Node::Text(netex_mode.to_string());
         Element::builder("TransportMode")
             .append(transport_mode_text)
             .build()
+    }
+
+    fn calculate_highest_mode(&self, netex_modes: &BTreeSet<NetexMode>) -> Option<NetexMode> {
+        // Since `BTreeSet is ordered, the first one in the list is of highest priority
+        netex_modes.iter().next().cloned()
     }
 
     fn generate_tariff_zones(&self, stop_point: &'a StopPoint) -> Option<Element> {
@@ -136,5 +384,35 @@ impl<'a> StopExporter<'a> {
                 .append(tariff_zone_ref)
                 .build()
         })
+    }
+
+    fn generate_quays<I, T>(&self, stop_point_ids: I) -> Element
+    where
+        I: IntoIterator<Item = T>,
+        T: Borrow<&'a str>,
+    {
+        let quays = stop_point_ids
+            .into_iter()
+            .map(|stop_point_id| self.generate_id(stop_point_id.borrow(), StopType::Quay, None))
+            .map(|quay_id| Element::builder("QuayRef").attr("ref", quay_id).build());
+        Element::builder("quays").append_all(quays).build()
+    }
+
+    fn generate_stop_place_type(&self, netex_mode: NetexMode) -> Element {
+        use NetexMode::*;
+        let stop_place_type = match netex_mode {
+            Air => "Airport",
+            Water => "ferryStop",
+            Rail => "railStation",
+            Metro => "metroStation",
+            Tram => "tramStation",
+            Funicular => "railStation",
+            Cableway => "liftStation",
+            Coach => "coachStation",
+            Bus => "busStation",
+        };
+        Element::builder("StopPlaceType")
+            .append(Node::Text(stop_place_type.to_owned()))
+            .build()
     }
 }
