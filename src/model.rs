892e90719e59a6128f115ec73df74023241d9f88
Antoine D <antoine.desbordes@gmail.com>
2021-01-13 08:48:32 +0000

    Update src/model.rs
    Co-authored-by: Jean SIMARD <woshilapin@tuziwo.info>

--- src/model.rs
+++ src/model.rs
@@ -1894,7 +1894,7 @@ mod tests {
         }
 
         #[test]
-        fn block_id_on_overlaping_calendar_ok() {
+        fn block_id_on_overlapping_calendar_ok() {
             // like the example 4 but on less days
             // working days:
             // days: 01 02 03 04
