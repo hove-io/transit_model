22b5e997f2a22ac8345244495f18bd9902ecccad
Jean SIMARD <woshilapin@tuziwo.info>
2021-02-18 18:32:32 +0100

    [fix] do not use 'EPSG:*' anymore
    'EPSG:*' creates a dependency on 'proj-data' which is harder to maintain. We're
    using the website epsg.io to know the exact string to use in replacement.

--- src/netex_france/exporter.rs
+++ src/netex_france/exporter.rs
@@ -171,8 +171,8 @@ impl<'a> Exporter<'a> {
     }
 
     pub(in crate::netex_france) fn get_coordinates_converter() -> Result<Proj> {
-        let from = "EPSG:4326";
-        let to = "EPSG:2154";
+        let from = "+proj=longlat +datum=WGS84 +no_defs"; // https://epsg.io/4326
+        let to = "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"; // https://epsg.io/2154
         Proj::new_known_crs(from, to, None)
             .ok_or_else(|| format_err!("Proj cannot build a converter from '{}' to '{}'", from, to))
     }
