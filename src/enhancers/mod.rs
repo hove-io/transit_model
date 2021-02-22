2fa142ce871f37b9a62ed73d960abf9adaf18998
ArnaudOggy <arnaud.ogiela@gmail.com>
2021-02-22 15:30:06 +0100

    [feature] Improve lines names

--- src/enhancers/mod.rs
+++ src/enhancers/mod.rs
@@ -1,5 +1,7 @@
 //! This module contains various functions that enhance / cleanup `Collections`
 
+mod adjust_lines_names;
 mod fill_co2;
 
+pub(crate) use adjust_lines_names::adjust_lines_names;
 pub(crate) use fill_co2::fill_co2;
