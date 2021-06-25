df668525bf3d05571f830ca415f306a1cbdada9b
Jean SIMARD <woshilapin@tuziwo.info>
2021-06-25 16:36:35 +0200

    [feature] output in ZIP format if extension is '.zip'

--- gtfs2ntfs/tests/gtfs2ntfs.rs
+++ gtfs2ntfs/tests/gtfs2ntfs.rs
@@ -35,3 +35,21 @@ fn test_gtfs2ntfs_create_output_directory() {
         .success();
     compare_output_dir_with_expected(&unexisting_dir, None, "tests/fixtures/output");
 }
+
+#[test]
+fn test_gtfs2ntfs_create_zip() {
+    let output_dir = TempDir::new().expect("create temp dir failed");
+    let ntfs_zip = output_dir.path().join("ntfs.zip");
+    assert!(!ntfs_zip.exists());
+    Command::cargo_bin("gtfs2ntfs")
+        .expect("Failed to find binary 'gtfs2ntfs'")
+        .arg("--input")
+        .arg("../tests/fixtures/gtfs2ntfs/minimal/input")
+        .arg("--output")
+        .arg(ntfs_zip.to_str().unwrap())
+        .arg("--current-datetime")
+        .arg("2019-04-03T17:19:00Z")
+        .assert()
+        .success();
+    assert!(ntfs_zip.is_file());
+}
