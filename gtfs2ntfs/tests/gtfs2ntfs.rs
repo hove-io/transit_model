03248e9edac1adad18f39b1a710f57bd0eda4c73
Jean SIMARD <woshilapin@tuziwo.info>
2021-06-25 15:33:04 +0200

    [test] add binary tests for 'gtfs2ntfs'

---
+++ gtfs2ntfs/tests/gtfs2ntfs.rs
@@ -0,0 +1,37 @@
+use assert_cmd::prelude::*;
+use std::process::Command;
+use tempfile::TempDir;
+use transit_model::test_utils::*;
+
+#[test]
+fn test_gtfs2ntfs() {
+    let output_dir = TempDir::new().expect("create temp dir failed");
+    Command::cargo_bin("gtfs2ntfs")
+        .expect("Failed to find binary 'gtfs2ntfs'")
+        .arg("--input")
+        .arg("../tests/fixtures/gtfs2ntfs/minimal/input")
+        .arg("--output")
+        .arg(output_dir.path().to_str().unwrap())
+        .arg("--current-datetime")
+        .arg("2019-04-03T17:19:00Z")
+        .assert()
+        .success();
+    compare_output_dir_with_expected(&output_dir, None, "tests/fixtures/output");
+}
+
+#[test]
+fn test_gtfs2ntfs_create_output_directory() {
+    let output_dir = TempDir::new().expect("create temp dir failed");
+    let unexisting_dir = output_dir.path().join("unexisting-folder");
+    Command::cargo_bin("gtfs2ntfs")
+        .expect("Failed to find binary 'gtfs2ntfs'")
+        .arg("--input")
+        .arg("../tests/fixtures/gtfs2ntfs/minimal/input")
+        .arg("--output")
+        .arg(unexisting_dir.to_str().unwrap())
+        .arg("--current-datetime")
+        .arg("2019-04-03T17:19:00Z")
+        .assert()
+        .success();
+    compare_output_dir_with_expected(&unexisting_dir, None, "tests/fixtures/output");
+}
