f8a633e736beda42dddbb966cf0342e4ef090cbf
Jean SIMARD <woshilapin@tuziwo.info>
2021-06-29 14:58:44 +0200

    [test] test for file presence instead of content in *2netexfr

--- gtfs2netexfr/tests/gtfs2netexfr.rs
+++ gtfs2netexfr/tests/gtfs2netexfr.rs
@@ -1,7 +1,6 @@
 use assert_cmd::prelude::*;
 use std::process::Command;
 use tempfile::TempDir;
-use transit_model::test_utils::*;
 
 #[test]
 fn test_gtfs2netexfr() {
@@ -18,20 +17,26 @@ fn test_gtfs2netexfr() {
         .arg("2019-04-03T17:19:00Z")
         .assert()
         .success();
-    compare_output_dir_with_expected(&output_dir, None, "../tests/fixtures/netex_france/output/");
-    let network_folders = std::fs::read_dir(output_dir)
-        .unwrap()
-        .map(|dir_entry| dir_entry.unwrap())
-        .map(|dir_entry| dir_entry.path())
-        .filter(|path| path.is_dir());
-    for network_folder in network_folders {
-        let folder_name = network_folder.file_name().unwrap();
-        let expected_folder = format!(
-            "../tests/fixtures/netex_france/output/{}",
-            folder_name.to_str().unwrap()
-        );
-        compare_output_dir_with_expected_content(&network_folder, None, &expected_folder);
-    }
+    assert!(output_dir.path().join("arrets.xml").is_file());
+}
+
+#[test]
+fn test_gtfs2netexfr_without_dir() {
+    let output_dir = TempDir::new().expect("create temp dir failed");
+    let unexisting_dir = output_dir.path().join("unexisting-dir");
+    Command::cargo_bin("gtfs2netexfr")
+        .expect("Failed to find binary 'gtfs2netexfr'")
+        .arg("--input")
+        .arg("../tests/fixtures/netex_france/input_gtfs")
+        .arg("--output")
+        .arg(unexisting_dir.to_str().unwrap())
+        .arg("--participant")
+        .arg("Participant")
+        .arg("--current-datetime")
+        .arg("2019-04-03T17:19:00Z")
+        .assert()
+        .success();
+    assert!(unexisting_dir.join("arrets.xml").is_file());
 }
 
 #[test]
